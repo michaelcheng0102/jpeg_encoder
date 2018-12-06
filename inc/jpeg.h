aafa0f3c67f17b249cd7c1d37f9e6f708f9c113b
Yen-Chieh Sung <ynchsung@gmail.com>
2018-12-05 19:17:20 -0600

    Remove JPEG class

--- inc/jpeg.h
+++ inc/jpeg.h
@@ -11,40 +11,23 @@
 #include "rle.h"
 #include "constants.h"
 
-class JPEG {
-private:
-	int width;
-	int height;
+void RGB2YCbCr(YUV& yuv, const BMP& bmp);
 
-	std::vector<std::vector<Block> > blks_y;
-	std::vector<std::vector<Block> > blks_cb;
-	std::vector<std::vector<Block> > blks_cr;
+int category_encode(int val);
 
-	QuanTable* qtab[16];
-	Huffman* hdc[16];
-	Huffman* hac[16];
+void fdct(double f[BLOCK_SIZE][BLOCK_SIZE], const double yuv_data[BLOCK_SIZE][BLOCK_SIZE]);
+void quantize(int f1[BLOCK_SIZE][BLOCK_SIZE], const double f2[BLOCK_SIZE][BLOCK_SIZE], YUV_ENUM type);
+void zigzag(int zz[BLOCK_SIZE * BLOCK_SIZE], const int f[BLOCK_SIZE][BLOCK_SIZE]);
+void rle(std::vector<RLE>& rle_list, const int zz[BLOCK_SIZE * BLOCK_SIZE]);
 
-	void RGB2YCbCr(YUV& yuv, const BMP& bmp);
+void go_transform_block(Block& blk, YUV_ENUM type);
+void encode(YUV &yuv);
 
-	int category_encode(int val);
+void write_huffman(int key, int val, int len, Huffman* table, YUV_ENUM type, FILE* fp, bool ff = true);
+void go_encode_block_to_file(Block& blk, int& dc, YUV_ENUM type, FILE* fp);
+void write_to_file(const char* output_path);
 
-	void fdct(double f[BLOCK_SIZE][BLOCK_SIZE], const double yuv_data[BLOCK_SIZE][BLOCK_SIZE]);
-	void quantize(int f1[BLOCK_SIZE][BLOCK_SIZE], const double f2[BLOCK_SIZE][BLOCK_SIZE], YUV_ENUM type);
-	void zigzag(int zz[BLOCK_SIZE * BLOCK_SIZE], const int f[BLOCK_SIZE][BLOCK_SIZE]);
-	void rle(std::vector<RLE>& rle_list, const int zz[BLOCK_SIZE * BLOCK_SIZE]);
-
-	void go_transform_block(Block& blk, YUV_ENUM type);
-	void encode(YUV &yuv);
-
-	void write_huffman(int key, int val, int len, Huffman* table, YUV_ENUM type, FILE* fp, bool ff = true);
-	void go_encode_block_to_file(Block& blk, int& dc, YUV_ENUM type, FILE* fp);
-	void write_to_file(const char* output_path);
-
-public:
-	JPEG();
-	~JPEG();
-
-	void convert_bmp_to_jpg(const char* input_path, const char* output_path);
-};
+void jpeg_init();
+void convert_bmp_to_jpg(const char* input_path, const char* output_path);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <omp.h>
 #include <cstdio>
 #include <cstdlib>
 #include "jpeg.h"
@@ -6,8 +5,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	printf("Thread %d\n", omp_get_thread_num ());
-	JPEG jpeg;
-	jpeg.convert_bmp_to_jpg(argv[1], argv[2]);
+	jpeg_init();
+	convert_bmp_to_jpg(argv[1], argv[2]);
 	return 0;
 }
